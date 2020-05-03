78a41bfb20a180f9d3af55fadd484ae0404db4d7
Nico <ncollie42@gmail.com>
2020-05-02 23:10:51 -0700

    Files structure, included vue

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 MagicLink
+Tmp notes--
+# vue creation
+vue create 'name'
+npm install google-protobuf @types/google-protobuf @improbable-eng/grpc-web --save
+npm install ts-protoc-gen
---
+++ vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <callgRPC/>
+  </div>
+</template>
+
+<script>
+import CallgRPC from "./components/CallgRPC.vue"
+export default {
+  name: 'App',
+  components: {
+    CallgRPC
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
