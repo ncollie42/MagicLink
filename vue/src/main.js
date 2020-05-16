8808d1cf098eb3825a4032e750dfd474b15ccd6d
Nico <ncollie42@gmail.com>
2020-05-16 15:00:18 -0700

    node_modules
    Testing on mac

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-// Vue.prototype.$hostname = "http://173.255.248.250:8080"
-Vue.prototype.$hostname = "http://10.10.134.112:8080"
+Vue.prototype.$hostname = "http://173.255.248.250:8080"
+// Vue.prototype.$hostname = "http://10.10.134.112:8080"
 new Vue({
   router,
   render: h => h(App)
