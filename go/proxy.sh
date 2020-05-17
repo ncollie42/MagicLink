fba38434137cb3c1c028d4757274c30421575d3c
Nico <ncollie42@gmail.com>
2020-05-16 21:11:52 -0700

    test build

--- go/proxy.sh
+++ go/proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grpcwebproxy --backend_addr=10.10.134.112:4242 --allow_all_origins=true --run_tls_server=false --server_http_max_read_timeout=11m --server_http_max_write_timeout=11m
+grpcwebproxy --backend_addr=10.10.134.112:4242 --allow_all_origins --run_tls_server=false --server_http_max_read_timeout=11m --server_http_max_write_timeout=11m
