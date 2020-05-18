f1cbfe7c488f2732917f554cffa70e1090477078
Nico <ncollie42@gmail.com>
2020-05-18 00:11:08 -0700

    trying to use tls

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.prototype.$hostname = "http://173.255.248.250:8080"
+Vue.prototype.$hostname = "https://173.255.248.250:8443"
 // Vue.prototype.$hostname = "http://10.10.134.112:8080"
 new Vue({
   router,
